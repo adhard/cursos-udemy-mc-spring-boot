3a7bbe15fac927327f3a86175f4252228a329140
adhard <adr.hardtke@outlook.com>
2021-02-13 13:59:01 -0300

    curso modelagem conceitual - modulo spring

---
+++ src/main/java/com/adriano/cursomcvscode/CursomcVscodeApplication.java
@@ -0,0 +1,13 @@
+package com.adriano.cursomcvscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcVscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcVscodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adriano/cursomcvscode/CursomcVscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adriano.cursomcvscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcVscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
